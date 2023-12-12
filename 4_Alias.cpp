7cbdac7d0ecf897507834b2a661a14583ac114c2
mllangon <mllangon@myuax.com>
2023-12-12 12:30:47 +0100

    4 3

--- 4_Alias.cpp
+++ 4_Alias.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
-#include<stdio.h>
-#include<string.h>
+#include <cstring>
 
 struct Estudiante {
     char nombre[50];
@@ -10,17 +9,34 @@ struct Estudiante {
 
 typedef struct Estudiante EstudianteAlias;
 
-int main() {
-    EstudianteAlias estudiante1;
+EstudianteAlias obtenerCopiaYMostrarAlias(const Estudiante& estudiante) {
 
+    std::cout << "Estudiante Original:" << std::endl;
+    std::cout << "Nombre: " << estudiante.nombre << std::endl;
+    std::cout << "Edad: " << estudiante.edad << std::endl;
+    std::cout << "Promedio: " << estudiante.promedio << std::endl;
+
+
+    EstudianteAlias copiaEstudiante;
+
+    strcpy(copiaEstudiante.nombre, estudiante.nombre);
+    copiaEstudiante.edad = estudiante.edad;
+    copiaEstudiante.promedio = estudiante.promedio;
+
+    std::cout << "\nAlias del Estudiante:" << std::endl;
+    std::cout << "Nombre: " << copiaEstudiante.nombre << std::endl;
+    std::cout << "Edad: " << copiaEstudiante.edad << std::endl;
+    std::cout << "Promedio: " << copiaEstudiante.promedio << std::endl;
+
+    return copiaEstudiante;
+}
+
+int main() {
+    Estudiante estudiante1;
     strcpy(estudiante1.nombre, "Juan");
     estudiante1.edad = 20;
     estudiante1.promedio = 8.5;
-
-    std::cout << "Estudiante 1:" << std::endl;
-    std::cout << "Nombre: " << estudiante1.nombre << std::endl;
-    std::cout << "Edad: " << estudiante1.edad << std::endl;
-    std::cout << "Promedio: " << estudiante1.promedio << std::endl;
+    obtenerCopiaYMostrarAlias(estudiante1);
 
     return 0;
 }
