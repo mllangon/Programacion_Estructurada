6e98bcd4fef65b917e59a297c3422e3fef49a5d3
mllangon <mllangon@myuax.com>
2023-12-12 11:57:35 +0100

    1 2

--- 1_Estructuras.cpp
+++ 1_Estructuras.cpp
@@ -2,8 +2,18 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct persona{
+struct Estudiante{
     char nombre[20];
     int edad;
-    float estatura;
+    float promedio;
 };
+
+int main(){
+    struct Estudiante estudiante1;
+    strcpy(estudiante1.nombre, "Mario");
+    estudiante1.edad = 18;
+    estudiante1.promedio = 8.3;
+
+    struct Estudiante* estudiantePtr
+
+}
