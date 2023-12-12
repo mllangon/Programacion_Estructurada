3a10decdaaadbd85aea40d4b3d97e2f45aa73420
mllangon <mllangon@myuax.com>
2023-12-12 12:04:13 +0100

    1 3

--- 1_Estructuras.cpp
+++ 1_Estructuras.cpp
@@ -12,17 +12,28 @@ int main(){
     struct Estudiante estudiante1;
     strcpy(estudiante1.nombre, "Mario");
     estudiante1.edad = 18;
-    estudiante1.promedio = 8.3;
+    estudiante1.promedio = 8.47;
 
-    struct Estudiante* estudiante2;
-    estudiante2 = (struct Estudiante*) malloc (sizeof(struct Estudiante));
-    strcpy(estudiante2->nombre, "Sergio");
-    estudiante2->edad = 19;
-    estudiante2->promedio = 5.3;
+    struct Estudiante* estudiantePtr;
+    estudiantePtr = (struct Estudiante*)malloc(sizeof(struct Estudiante));
+    if (estudiantePtr == nullptr) {
+        fprintf(stderr, "Error al asignar memoria.\n");
+        return 1;
+    }
+    strcpy(estudiantePtr->nombre, "Sergio");
+    estudiantePtr->edad = 19;
+    estudiantePtr->promedio = 5.38;
 
+    printf("Estudiante 1:\n");
+    printf("Nombre: %s\n", estudiante1.nombre);
+    printf("Edad: %d\n", estudiante1.edad);
+    printf("Promedio: %.2f\n\n", estudiante1.promedio);
 
+    printf("Estudiante 2:\n");
+    printf("Nombre: %s\n", estudiantePtr->nombre);
+    printf("Edad: %d\n", estudiantePtr->edad);
+    printf("Promedio: %.2f\n\n", estudiantePtr->promedio);
 
-
-
+    free(estudiantePtr);
 
 }
