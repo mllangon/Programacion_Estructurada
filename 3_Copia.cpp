3d436ae114b59348950c90942438f0b86a3cfe2d
mllangon <mllangon@myuax.com>
2023-12-12 12:20:47 +0100

    3 2

--- 3_Copia.cpp
+++ 3_Copia.cpp
@@ -17,3 +17,23 @@ Estudiante copiarEstudiante(const Estudiante& original) {
     return copia;
 }
 
+int main() {
+    Estudiante estudiante1;
+    strcpy(estudiante1.nombre, "Juan");
+    estudiante1.edad = 20;
+    estudiante1.promedio = 8.5;
+
+    Estudiante copiaEstudiante1 = copiarEstudiante(estudiante1);
+
+    std::cout << "Estudiante 1:" << std::endl;
+    std::cout << "Nombre: " << estudiante1.nombre << std::endl;
+    std::cout << "Edad: " << estudiante1.edad << std::endl;
+    std::cout << "Promedio: " << estudiante1.promedio << std::endl << std::endl;
+
+    std::cout << "Copia del Estudiante 1:" << std::endl;
+    std::cout << "Nombre: " << copiaEstudiante1.nombre << std::endl;
+    std::cout << "Edad: " << copiaEstudiante1.edad << std::endl;
+    std::cout << "Promedio: " << copiaEstudiante1.promedio << std::endl;
+
+    return 0;
+}
