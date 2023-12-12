f918372a61ee7fd1a5d643c1325b31283d60ec0f
mllangon <mllangon@myuax.com>
2023-12-12 12:10:57 +0100

    2 2

--- 2_Uniones.cpp
+++ 2_Uniones.cpp
@@ -7,3 +7,17 @@ union Datos{
     char Caracter[30];
 };
 
+int main(){
+    union Datos uni;
+
+    uni.Entero = 33;
+    printf("Entero: %d\n", uni.Entero);
+
+    uni.Flotante = 3.1416;
+    printf("Flotante: %.2f\n", uni.Flotante);
+
+    strcpy(uni.Caracter, "Hola buenos dias");
+    printf("Caracter: %s\n", uni.Caracter);
+
+    return 0;
+}
