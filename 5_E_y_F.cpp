b361f099693c69d30aaaaaaf0b318f855cccd851
mllangon <mllangon@myuax.com>
2023-12-12 12:34:43 +0100

    5 1

---
+++ 5_E_y_F.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstring>
+
+struct Estudiante {
+    char nombre[50];
+    int edad;
+    float promedio;
+};
+
+void imprimirEstudiantePorValor(Estudiante estudiante) {
+    std::cout << "Nombre: " << estudiante.nombre << std::endl;
+    std::cout << "Edad: " << estudiante.edad << std::endl;
+    std::cout << "Promedio: " << estudiante.promedio << std::endl;
+}
+
