3ace7fb61c219531552794917307a1ded64a630f
mllangon <mllangon@myuax.com>
2023-12-12 12:34:54 +0100

    5 2

--- 5_E_y_F.cpp
+++ 5_E_y_F.cpp
@@ -13,3 +13,15 @@ void imprimirEstudiantePorValor(Estudiante estudiante) {
     std::cout << "Promedio: " << estudiante.promedio << std::endl;
 }
 
+void imprimirEstudiantePorReferencia(const Estudiante& estudiante) {
+    std::cout << "Nombre: " << estudiante.nombre << std::endl;
+    std::cout << "Edad: " << estudiante.edad << std::endl;
+    std::cout << "Promedio: " << estudiante.promedio << std::endl;
+}
+
+void imprimirEstudiantePorDireccion(const Estudiante* estudiante) {
+    std::cout << "Nombre: " << estudiante->nombre << std::endl;
+    std::cout << "Edad: " << estudiante->edad << std::endl;
+    std::cout << "Promedio: " << estudiante->promedio << std::endl;
+}
+
